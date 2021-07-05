0201a7a6bf209a65e6df586b80174fa235983aa4
Fabio Junior Barbosa <fabiojbarbosa1@gmail.com>
2021-07-05 14:39:55 -0300

    Mover cactos.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
 const dino = document.querySelector(".dino");
-let jumping = false;
+const background = document.querySelector('.container-game');
+
+let jumping = false, walking = false, live = true;
 
 function handleKeyUp(){
     if(event.keyCode === 32 && !jumping){
         jumping = true;
-        moveUp();
+        moveUp(dino, 0, 85, 8);
     }
 }
 
-function moveUp() {
-    let position = 0;
+function moveUp(element, position_start, position_end, velocity) {
+    let position = position_start;
     let upInterval = setInterval(()=>{
-        if(position <= 100){
-            position += 10;
-            dino.style.bottom = position+'px';
+        if(position <= position_end){
+            position += velocity;
+            element.style.bottom = position+'px';
         }else{
             clearInterval(upInterval);
-            moveDown(position);
+            moveDown(dino, position, 0, velocity);
         }
     },25);
 }
 
-function moveDown(start_position) {
-    let position = start_position;
+function moveDown(element, position_start, position_end, velocity) {
+    let position = position_start;
     let upInterval = setInterval(()=>{
-        if(position > 0){
-            position -= 10;
-            dino.style.bottom = position+'px';
+        if(position > position_end){
+            position -= velocity;
+            element.style.bottom = position+'px';
         }else{
             clearInterval(upInterval);
             jumping = false;
@@ -34,6 +36,45 @@ function moveDown(start_position) {
     },25);
 }
 
-document.addEventListener('keyup', handleKeyUp)
+function moveRight(element, position_start, position_end, velocity) {
+    let position = position_start;
+    let upInterval = setInterval(()=>{
+        if(position <= position_end){
+            position += velocity;
+            element.style.right = position+'px';
+        }else{
+            clearInterval(upInterval);
+            moveDown(position);
+            background.removeChild(element);
+        }
+    },25);
+}
 
-console.log(dino);
+function gameover(){
+    console.log("Game Over")
+}
+
+createCactus=() => {
+        const intervalTime = Math.random()*6000;
+        setTimeout(() => {
+            if(live){
+                const cactus = document.createElement('div');
+                cactus.classList.add('cactus');
+                
+                moveRight(cactus, 0, screen.width, 5);
+
+                background.appendChild(cactus);
+                createCactus();
+            }else{
+                clearInterval(intervalTime);
+                gameover();
+            }
+        }, intervalTime);
+}
+
+function start(){
+    createCactus();
+    document.addEventListener('keyup', handleKeyUp);
+}
+
+start();
